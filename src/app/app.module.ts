341d82438b16a26ecc7a5080fb0386d40b78b577
Joseph Dara <joelutimehin@gmail.com>
2021-06-04 15:45:39 +0100

    o

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SampleComponentComponent } from './sample-component/sample-component.component';
+import { SampleComponent } from './sample/sample.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SampleComponentComponent
+    SampleComponent
   ],
   imports: [
     BrowserModule,
